e4595a4d32879d21c096543ce745636b1d66c9f6
Geymark-sw <emmanuelpapio12@gmail.com>
2024-10-07 13:47:23 +0200

    creazione repository java

---
+++ P20241007_02_Secondo/src/Main.java
@@ -0,0 +1,27 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Prima riga");
+		System.out.println("Seconda riga");
+		System.out.println("Terza riga");
+
+		byte b1 = 10;
+		byte b2;
+		b2 = 100;
+		byte b3, b4, b5;
+
+		System.out.println("Il valore di b1 è: " + b1);
+		
+		double d1 = 2.3;
+		float f1 =  2.3f;
+		
+		int v1 = 35;
+		int v2 = 41;
+		
+		System.out.println(v1 & v2);
+		
+	}
+
+}
